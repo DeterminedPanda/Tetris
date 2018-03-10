97584cb6703e5bc1804f7e222c4f8daa16e08883
DeterminedPanda <justinas.maniscalco@live.de>
2018-03-10 19:54:31 +0100

    initial commit

---
+++ main.lua
@@ -0,0 +1,16 @@
+io.write("Starting...\n");
+
+function love.load() 
+    io.write("load\n");
+
+end
+
+function love.update(dt) 
+    io.write("update\n");
+
+end
+
+function love.draw() 
+    io.write("draw\n");
+
+end
